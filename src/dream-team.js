6999990b4020c00171a3d00a4df625cde6a4a36e
newfolder.rar <mdouzhyk@gmail.com>
2022-10-17 00:35:21 +0400

    feat: dream-team task finished

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let secretName = []
+  if (members instanceof Array && members !== null) {
+    members.forEach(function (member) {
+      if (typeof member === 'string') {
+        secretName.push(member.trim()[0].toUpperCase())
+      }
+    })
+  } else {
+    return false
+  }
+  return secretName.sort().join('')
 }
 
 module.exports = {
